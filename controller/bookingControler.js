1d0ee59f8cc535a4d777f97c8bfa8cd367c7362d
BeringeloF <sasorim54@gmail.com>
2024-07-06 10:51:52 -0300

    i think now it s finished

--- controller/bookingControler.js
+++ controller/bookingControler.js
@@ -84,7 +84,7 @@ const createBookingCheckout = async (session) => {
   console.log(session);
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email }))._id;
-  const price = session.line_items[0].price_data.unit_amount / 100;
+  const price = session.amount_total / 100;
 
   await Booking.create({
     tour,
